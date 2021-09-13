31b7c9d408a1b606e983231a7c4508390a91a13f
Robert Nascimento <robert1a7x@gmail.com>
2021-09-13 18:32:22 -0300

    Exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import NavList from './NavList';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1> Pokedex </h1>
+        <NavList />
+        <Route exact path="/" component={(props) => <Pokedex {...props} pokemons={pokemons} />} />
+      </div>
+    </BrowserRouter>
   );
 }
 
